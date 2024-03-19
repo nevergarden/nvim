c65f9bd573612a169a531143bfc01e6102ffa6e8
Amir Rezai <aemorax@gmail.com>
2024-03-19 19:23:11 +0330

    add: jsonls

---
+++ lua/aemorax/lsp/jsonls.lua
@@ -0,0 +1,11 @@
+local lspconfig_status_ok, lspconfig = pcall(require, 'lspconfig')
+if not lspconfig_status_ok then
+	return
+end
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+lspconfig['jsonls'].setup({
+	capabilities = capabilities,
+})
